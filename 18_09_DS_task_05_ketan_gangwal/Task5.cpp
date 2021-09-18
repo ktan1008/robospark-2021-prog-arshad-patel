6b874e39081e60f9341ca08923c22777d0254615
Ketan Gangwal <ketan.gangwal20@vit.edu>
2021-09-18 15:40:26 +0530

    Updated commit Task 5 Submission Ketan-Gangwal

--- 18_09_DS_task_05_ketan_gangwal/Task5.cpp
+++ 18_09_DS_task_05_ketan_gangwal/Task5.cpp
@@ -2,43 +2,67 @@
 using namespace std;
 class Rectangle
 {
-private:
-    int length, breadth,x;
-
 public:
-    Rectangle(int a, int b)           // parameterized constructor with two parameters
+    int length, breadth, x;
+
+    Rectangle(int a, int b) // parameterized constructor with two parameters
     {
         length = a;
         breadth = b;
     }
-    void area()                          //function to find area
+    void area() //function to find area
     {
         x = length * breadth;
-        
-        cout<<"The area of rectangle is :"<<x<<endl;
+
+        cout << "The area of rectangle is :" << x << endl;
     }
 
-    Rectangle()                        // default Constructor
+    Rectangle() // default Constructor
     {
-        length=0;
-        breadth=0;
+        length = 0;
+        breadth = 0;
     }
 
-    Rectangle(int a)                   // parameterized constructor with one parameter
+    Rectangle(int a) // parameterized constructor with one parameter
     {
-        length=a;
-        breadth=a;
+        length = a;
+        breadth = a;
     }
 };
 
 int main()
 {
-    Rectangle r1;                   // Creating 1st object of Class Rectangle
-    r1.area();
-    Rectangle r2(3,5);              // Creating 2nd object of Class Rectangle with two parameters
-    r2.area();
-    Rectangle r3(5);                // Creating 3rd object of Class Rectangle with one parameter
-    r3.area();
-    
+    cout << "What kind of Constructor do you want to immplement :" << endl;
+    cout << "1-Default constructor" << endl;
+    cout << "2-Parametrized Constructor with Two parameters" << endl;
+    cout << "3-Parametrized Constructor with One parameter" << endl;
+
+    int option = 3;
+    cin >> option;
+
+    switch (Option)
+    {
+    case 1:
+        Rectangle r1;
+        r1.area;
+        break;
+
+    case 2:
+        float l, b;
+        cout<<"Enter the value of length and breadth"<<endl;
+        cin>>l>>b>>endl;
+        Rectangle r2(l,b);
+        r2.area();
+        break;
+
+    case 3:
+        float a;
+        cout<<"Enter the value of length=breadth"<<endl;
+        cin>>a;
+        Rectangle r3(a);
+        r3.area();
+        break;
+
+    }
     return 0;
 }
